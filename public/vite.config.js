fc4e09f65c59cd4ee79a33e87c6ee070f568caeb
Mitch <mitchmbagha@gmail.com>
2026-05-04 20:02:30 +0100

    "chore: setup github actions deployment for vite again"

---
+++ public/vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // This MUST match your exact GitHub repository name exactly, with the slashes
+  base: '/SpatialFFEConfigurator', 
+});
